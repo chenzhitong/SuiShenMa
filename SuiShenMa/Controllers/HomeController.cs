1a3ae91c914507ecddd202fb868dab6774a7f190
陈志同 <chenzhitong12@hotmail.com>
2020-03-03 18:09:37 +0800

    添加项目文件。

---
+++ SuiShenMa/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuiShenMa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Check(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
